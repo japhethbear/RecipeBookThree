a494544157925eb9321b9ccab4f5f1da78ceff2b
japhethbear <japhethbear@gmail.com>
2024-02-29 09:37:43 -0500

    Fixed Prisma Client Import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import Link from 'next/link';
 import ProductCard from './users/components/ProductCard/ProductCard';
+import prisma from '@/prisma/prisma';
 
-export default function Home() {
+async function getPosts() {
+  const posts = await prisma.post.findMany({
+    where: { published: true}, 
+    include: { 
+      author: {
+        select: {name: true}
+      }
+    }
+  })
+  return posts;
+}
+
+export default async function Home() {
+  
+  const posts = await getPosts();
+  console.log({posts});
+  
   return (
     <main>
       <h1>Hello World</h1>
